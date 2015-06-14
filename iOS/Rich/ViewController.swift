47b45c7d4e27c9fd0be5ab5b44172a0a5781c578
Brian Vallelunga <vallelungabrian@gmail.com>
2015-06-13 19:41:40 -0700

    v2

--- iOS/Rich/ViewController.swift
+++ iOS/Rich/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // MARK: Instance Variables
-    var shimmerView: FBShimmeringView!
+    // MARK: IBOutlets
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var shimmerView: FBShimmeringView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,20 +20,15 @@ class ViewController: UIViewController {
         var background = UIImageView(frame: self.view.frame)
         background.image = UIImage(named: "Background")
         background.contentMode = UIViewContentMode.ScaleAspectFill
-        self.view.addSubview(background)
+        self.view.insertSubview(background, atIndex: 0)
         
-        var label = UILabel(frame: self.view.frame)
-        label.textAlignment = NSTextAlignment.Center
-        label.text = "R$CH"
-        label.textColor = UIColor(red:0.31, green:0.26, blue:0.12, alpha:1)
-        label.font = UIFont(name: "AmericanTypewriter-Bold", size: 100)
-        label.shadowOffset = CGSizeMake(0, 4)
-        label.shadowColor = UIColor(red:0.46, green:0.37, blue:0.15, alpha:1)
+        self.label.textAlignment = NSTextAlignment.Center
+        self.label.textColor = UIColor(red:0.31, green:0.26, blue:0.12, alpha:1)
+        self.label.shadowOffset = CGSizeMake(0, 4)
+        self.label.shadowColor = UIColor(red:0.46, green:0.37, blue:0.15, alpha:1)
         
-        self.shimmerView = FBShimmeringView(frame: self.view.frame)
         self.shimmerView.contentView = label
         self.shimmerView.shimmering = true
-        self.view.addSubview(self.shimmerView)
     }
 }
 
